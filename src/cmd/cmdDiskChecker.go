ba2726193ccaf6918fa14387220280c357e78b1f
guoxu@121.42.8.74 <13910754971@163.com>
2018-08-10 16:52:27 +0800

    1

---
+++ src/cmd/cmdDiskChecker.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"diskChecker"
+	"sync"
+	"flag"
+)
+
+var wgs []*sync.WaitGroup
+
+func registerSubUtil(wg *sync.WaitGroup)  {
+	wgs = append(wgs, wg)
+}
+
+func main() {
+	path := flag.String("foder","F:\\","Folder to monitor.")
+	period := flag.Int("period", 5, "The period to check the folder size.")
+	//checker := diskChecker.MakeDiskChecker("F:\\wanchain_with_istanbul",2)
+	checker := diskChecker.MakeDiskChecker(*path,*period)
+	wg := checker.Start()
+	registerSubUtil(wg)
+	for _,wg := range wgs {
+		wg.Wait()
+	}
+}
